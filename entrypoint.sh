57a6e9a8e83c49ce9644d3aba869920804d01123
brian <brian770130@gmail.com>
2020-12-10 20:29:53 +0800

    add abci application

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ export TMHOME="/tendermint/node${ID}"
 linenum=$(grep -n "TCP or UNIX socket address for the RPC server to listen on" ${TMHOME}/config/config.toml | awk -F':' '{print $1}')
 ((linenum++))
 sed -i "${linenum}c laddr = \"tcp://0.0.0.0:26657\"" ${TMHOME}/config/config.toml
-./tendermint node --home ${TMHOME} --proxy_app kvstore
+./tendermint node --home ${TMHOME} --proxy_app "tcp://abci${ID}:26658"
 
