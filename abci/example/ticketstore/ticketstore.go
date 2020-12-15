7ceb46651a88135d4f8ff30c19184b4abc311334
brian <brian770130@gmail.com>
2020-12-15 14:23:01 +0800

    fix deliver tx and make it work

--- abci/example/ticketstore/ticketstore.go
+++ abci/example/ticketstore/ticketstore.go
@@ -1,88 +1,74 @@
 package ticketstore
 
 import (
-	"encoding/binary"
 	"encoding/json"
 	"fmt"
-	"strconv"
-	"strings"
 
-	"github.com/cbergoon/merkletree"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto"
-	sha3 "github.com/miguelmota/go-solidity-sha3"
 	"github.com/tendermint/tendermint/abci/types"
 )
 
 const (
 	codeTypeOK            uint32 = 0
 	codeTypeEncodingError uint32 = 1
-	codeTypeTicketError   uint32 = 2
+	codeTypeModelError    uint32 = 2
 )
 
 var (
-	ErrBadAddress     = &ticketError{"Ticket must have an address"}
-	ErrBadNonce       = &ticketError{"Ticket nonce must increase on resale"}
-	ErrBadSignature   = &ticketError{"Resale must be signed by the previous owner"}
-	ErrTicketNotFound = &ticketError{"Ticket could not be found"}
+	ErrBadRound       = "The round number is not match"
+	ErrConflict       = "The model round number has been used"
 )
-
-type ticketError struct{ msg string }
-
-func (err ticketError) Error() string { return err.msg }
 
 type TicketStoreApplication struct {
 	types.BaseApplication
-	state state
+	state State
 }
 
-type state struct {
-	size            int64
-	height          int64
-	rootHash        []byte
-	tickets         map[uint64]ticket
-	history         map[int64]snapshot
-	tempTreeContent []merkletree.Content
+type State struct {
+	aggregatedModel Model
+	historyModel map[uint64]Snapshot
+	height uint64
+	round  uint64
+	clientsNumber int
+	rootHash []byte
 }
 
-type TicketTx struct {
-	Id             uint64 `json:"id"`
-	Nonce          uint64 `json:"nonce"`
-	Details        string `json:"details"`
-	OwnerAddr      string `json:"ownerAddr"`
-	PrevOwnerProof string `json:"prevOwnerProof"`
+type Model struct {
+	weight float64
 }
 
-type ticketResponse struct {
-	Ticket      ticket   `json:"ticket"`
-	MerkleProof []string `json:"merkleProof"`
-	Index       []int64  `json:"index"`
+type Snapshot struct {
+	ready bool
+	localModels map[uint64]Model
+	update map[uint64]bool
 }
 
-type ticket struct {
-	TicketTx      `json:"ticketTx"`
-	ChangeHeights []int64 `json:"changeHeights"`
-}
-
-type snapshot struct {
-	tickets map[uint64]ticket
-	tree    merkletree.MerkleTree
+type ModelTx struct {
+	Round		uint64	`json:"round"`
+	Weight		float64 `json:"weight"`
+	CID			uint64 	`json:"cid"`
+	Signature 	string 	`json:"signature"`
 }
 
 func NewTicketStoreApplication() *TicketStoreApplication {
-	return &TicketStoreApplication{state: state{tickets: make(map[uint64]ticket), history: make(map[int64]snapshot)}}
+	return &TicketStoreApplication{
+		state: State{ aggregatedModel: Model{ weight: 0.0 },
+					  historyModel: make( map[uint64]Snapshot ),
+					  clientsNumber: 4,
+					},
+		}
 }
 
 func (app *TicketStoreApplication) Info(req types.RequestInfo) types.ResponseInfo {
 	return types.ResponseInfo{
-		Data:             fmt.Sprintf("{\"hashes\":%v,\"tickets\":%v}", app.state.height, app.state.size),
-		LastBlockHeight:  app.state.height,
-		LastBlockAppHash: app.state.rootHash}
+		Data:             fmt.Sprintf("{\"round\":%v, \"model\":%v}", app.state.round, app.state.aggregatedModel.weight),
+		LastBlockHeight:  int64(app.state.height),
+		LastBlockAppHash: app.state.rootHash,
+	}
 }
 
 func (app *TicketStoreApplication) DeliverTx(tx types.RequestDeliverTx) types.ResponseDeliverTx {
-	var ticketTx TicketTx
-	err := json.Unmarshal(tx.Tx, &ticketTx)
+	var modelTx ModelTx
+	err := json.Unmarshal(tx.Tx, &modelTx)
 
 	if err != nil {
 		return types.ResponseDeliverTx{
@@ -90,57 +76,54 @@ func (app *TicketStoreApplication) DeliverTx(tx types.RequestDeliverTx) types.Re
 			Log:  fmt.Sprint(err)}
 	}
 
-	previousTicket := app.state.tickets[ticketTx.Id]
-	err = ticketTx.validate(previousTicket.TicketTx)
-	if err != nil {
+	nextRound := app.state.round + 1
+
+	if modelTx.Round != nextRound {
 		return types.ResponseDeliverTx{
-			Code: codeTypeTicketError,
-			Log:  fmt.Sprint(err)}
+			Code: codeTypeModelError,
+			Log: fmt.Sprint(ErrBadRound)}
 	}
 
-	app.state.size++
-	changeHeights := append(previousTicket.ChangeHeights, app.state.height+1)
-	app.state.tickets[ticketTx.Id] = ticket{ticketTx, changeHeights}
-	app.state.tempTreeContent = append(app.state.tempTreeContent, ticketTx)
+	cid := modelTx.CID
+	if app.state.historyModel[nextRound].ready == false {
+		// fmt.Printf("initial struct")
+		app.state.historyModel[nextRound] = Snapshot{ ready: true,
+													  localModels: make( map[uint64]Model ),
+													  update: make( map[uint64]bool )}
+	}
+	if app.state.historyModel[nextRound].update[cid] {
+		return types.ResponseDeliverTx{
+			Code: codeTypeModelError,
+			Log: fmt.Sprint(ErrConflict)}
+	}
+	app.state.historyModel[nextRound].update[cid] = true
+	app.state.historyModel[nextRound].localModels[cid] = Model{weight: modelTx.Weight}
 	return types.ResponseDeliverTx{Code: codeTypeOK}
 }
 
 func (app *TicketStoreApplication) CheckTx(tx types.RequestCheckTx) types.ResponseCheckTx {
-	var ticketTx TicketTx
-	err := json.Unmarshal(tx.Tx, &ticketTx)
-	fmt.Printf("Debug: %+v \n", string(tx.Tx))
-
-	if err != nil {
-		return types.ResponseCheckTx{
-			Code: codeTypeEncodingError,
-			Log:  fmt.Sprint(err)}
-	}
-
-	previousTicket := app.state.tickets[ticketTx.Id]
-	err = ticketTx.validate(previousTicket.TicketTx)
-	if err != nil {
-		return types.ResponseCheckTx{
-			Code: codeTypeTicketError,
-			Log:  fmt.Sprint(err)}
-	}
-
 	return types.ResponseCheckTx{Code: codeTypeOK}
 }
 
 func (app *TicketStoreApplication) Commit() (resp types.ResponseCommit) {
-	app.state.height++
-	if len(app.state.tempTreeContent) > 0 {
-		tree, _ := merkletree.NewTree(app.state.tempTreeContent)
-		app.state.rootHash = tree.Root.Hash
-		ticketsSnapshot := make(map[uint64]ticket)
-		for key, value := range app.state.tickets {
-			ticketsSnapshot[key] = value
+	allClientsUpdate := true
+	nextRound := app.state.round + 1
+
+	for i := 0 ; i < app.state.clientsNumber; i++ {
+		if app.state.historyModel[nextRound].update[uint64(i)] == false {
+			allClientsUpdate = false
 		}
-		app.state.history[app.state.height] = snapshot{ticketsSnapshot, *tree}
-		app.state.tempTreeContent = app.state.tempTreeContent[:0]
 	}
 
-	return types.ResponseCommit{Data: app.state.rootHash}
+	modelsNextRound := app.state.historyModel[nextRound].localModels
+
+	if allClientsUpdate {
+		app.state.aggregatedModel = Model{weight: AggregateModel(modelsNextRound, app.state.clientsNumber)}
+		app.state.round++
+	}
+
+	app.state.height++
+	return types.ResponseCommit{Data: []byte{0x00}}
 }
 
 func (app *TicketStoreApplication) Query(reqQuery types.RequestQuery) types.ResponseQuery {
@@ -149,140 +132,21 @@ func (app *TicketStoreApplication) Query(reqQuery types.RequestQuery) types.Resp
 	fmt.Printf("Debug: %v\n", string(reqQuery.Height))
 	fmt.Printf("Debug: %v\n", reqQuery.Prove)
 	switch reqQuery.Path {
-	case "hash":
-		return types.ResponseQuery{Value: []byte(fmt.Sprint(app.state.height))}
-	case "tx":
-		return types.ResponseQuery{Value: []byte(fmt.Sprint(app.state.size))}
-	case "ticket":
-		ticketResponse, err := app.state.findTicket(reqQuery)
-		if err != nil {
-			return types.ResponseQuery{Log: fmt.Sprintf("%v is not a valid ticket id", reqQuery.Data)}
-		}
-		response, _ := json.Marshal(ticketResponse)
-		return types.ResponseQuery{Value: response}
+	case "round":
+		return types.ResponseQuery{Value: []byte(fmt.Sprint(app.state.round))}
+	case "weight":
+		return types.ResponseQuery{Value: []byte(fmt.Sprint(app.state.aggregatedModel.weight))}
+	case "clients":
+		return types.ResponseQuery{Value: []byte(fmt.Sprint(app.state.clientsNumber))}
 	default:
 		return types.ResponseQuery{Log: fmt.Sprintf("Invalid query path. Expected hash, tx or ticket, got %v", reqQuery.Path)}
 	}
 }
 
-func (ticket TicketTx) CalculateHash() ([]byte, error) {
-	idBytes := make([]byte, 8)
-	binary.LittleEndian.PutUint64(idBytes, ticket.Id)
-	hash := sha3.SoliditySHA3(
-		[]string{"uint256", "uint256", "string", "address", "bytes"},
-		[]interface{}{fmt.Sprint(ticket.Id), fmt.Sprint(ticket.Nonce), ticket.Details, ticket.OwnerAddr, ticket.PrevOwnerProof})
-	return hash, nil
-}
-
-func (ticket TicketTx) Equals(other merkletree.Content) (bool, error) {
-	otherTicket, isTicket := other.(TicketTx)
-	if isTicket {
-		return ticket == otherTicket, nil
+func AggregateModel(localModels map[uint64]Model, clientsNumber int) (float64) {
+	sum := 0.0
+	for i := 0; i < clientsNumber; i++ {
+		sum += localModels[uint64(i)].weight
 	}
-
-	return false, fmt.Errorf("%v is not a ticket", other)
-}
-
-func (ticket TicketTx) validate(prevTicket TicketTx) error {
-	if ticket.OwnerAddr == "" {
-		return ErrBadAddress
-	}
-
-	if ticket.Nonce <= prevTicket.Nonce {
-		return ErrBadNonce
-	}
-
-	if prevTicket.OwnerAddr != "" {
-		prevTicketHash, err := prevTicket.CalculateHash()
-		if err != nil {
-			return err
-		}
-
-		signer, err := ticket.getOwnerProofSigner(prevTicketHash)
-		if err != nil {
-			return err
-		}
-		if signer != strings.ToLower(prevTicket.OwnerAddr) {
-			return ErrBadSignature
-		}
-	}
-
-	return nil
-}
-
-func (ticket TicketTx) getOwnerProofSigner(prevTicketHash []byte) (string, error) {
-	if len(ticket.PrevOwnerProof) < 3 {
-		// Cannot be a valid proof
-		return "", ErrBadSignature
-	}
-
-	bytesProof, err := hexutil.Decode(ticket.PrevOwnerProof)
-	if err != nil {
-		return "", err
-	}
-
-	bytesProof[64] -= 27
-	signerPkey, err := crypto.SigToPub(prevTicketHash, bytesProof)
-	if err != nil {
-		return "", err
-	}
-
-	return strings.ToLower(crypto.PubkeyToAddress(*signerPkey).Hex()), nil
-}
-
-func (state state) findTicket(query types.RequestQuery) (ticketResponse, error) {
-	ticketId, err := strconv.ParseUint(string(query.Data), 10, 64)
-	if err != nil {
-		return ticketResponse{}, err
-	}
-
-	height := query.Height
-	if height <= 0 {
-		height = state.height
-	}
-
-	lastTicketChange, err := state.tickets[ticketId].findLastChangeBeforeHeight(height)
-	if err != nil {
-		return ticketResponse{}, err
-	}
-
-	snapshot := state.history[lastTicketChange]
-	ticket := snapshot.tickets[ticketId]
-	merkleProofBytes, index, err := snapshot.tree.GetMerklePath(ticket.TicketTx)
-	if err != nil {
-		return ticketResponse{}, err
-	}
-
-	merkleProof := make([]string, len(merkleProofBytes))
-	for i, v := range merkleProofBytes {
-		merkleProof[i] = hexutil.Encode(v)
-	}
-	return ticketResponse{Ticket: ticket, Index: index, MerkleProof: merkleProof}, nil
-}
-
-func parseTicketQuery(queryData string, currentHeight int64) (ticketId uint64, height int64, err error) {
-	params := strings.Split(queryData, ":")
-	ticketId, err = strconv.ParseUint(params[0], 10, 64)
-	if err != nil {
-		return
-	}
-
-	// Height was not provided so assume latest
-	if len(params) == 1 {
-		height = currentHeight
-		return
-	}
-
-	height, err = strconv.ParseInt(params[1], 10, 64)
-	return
-}
-
-func (ticket ticket) findLastChangeBeforeHeight(height int64) (int64, error) {
-	for i := len(ticket.ChangeHeights) - 1; i >= 0; i-- {
-		if ticket.ChangeHeights[i] <= height {
-			return ticket.ChangeHeights[i], nil
-		}
-	}
-
-	return 0, ErrTicketNotFound
+	return sum / float64(clientsNumber)
 }
